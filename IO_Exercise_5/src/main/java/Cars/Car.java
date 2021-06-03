4d157b78ce3137368ea2df65f1422fc93fa8bbbc
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2021-06-03 07:21:51 +0200

    Car class done.

---
+++ IO_Exercise_5/src/main/java/Cars/Car.java
@@ -0,0 +1,59 @@
+package Cars;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Car implements Serializable {
+
+    private String carReg;
+    private String model;
+    private String brand;
+    private int carId;
+
+    Car(){}
+
+    public String getCarReg() {
+        return carReg;
+    }
+
+    public void setCarReg(String carReg) {
+        this.carReg = carReg;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(carReg, car.carReg) && Objects.equals(model, car.model) && Objects.equals(brand, car.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carReg, model, brand);
+    }
+}
